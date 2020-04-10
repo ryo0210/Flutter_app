70edd2842b6b0255c3fd1f4c1401cd4a435c6a2e
ryo0210 <yone0217@icloud.com>
2020-04-10 20:24:52 +0900

    Initialize the project. Added the text of storyTitle and choice.

---
+++ distini/story.dart
@@ -0,0 +1,10 @@
+//Step 2 - クラスを作成.
+//Step 3 -  Storyクラスのプロパティを作成.
+//Step 4 - Storyクラスのコンストラクタを作成.
+class Story {
+  String storyTitle;
+  String choice1;
+  String choice2;
+
+  Story({this.storyTitle, this.choice1, this.choice2});
+}
